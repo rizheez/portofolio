20a8bf43106d1db9cbdbe9e1f044c62da8b34d5e
rizheez <rezamuhammad980@gmail.com>
2023-03-24 22:23:10 +0800

    updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,9 @@ import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 import { ava } from "../assets/index";
 function Home() {
     return (
-        <div
-            className="flex items-center justify-center"
-        >
-            <div className=" text-white" data-aos="zoom-out" data-aos-delay="250">
-                <div className="flex flex-col content-center items-center pt-40">
+        <div>
+            <div className=" text-white" data-aos="flip-left" data-aos-delay="200">
+                <div className="flex flex-col items-center pt-40">
                     <img className="w-52" src={ ava } alt="avatar" />
                     <h1 className="text-4xl font-bold mb-4 text-center pt-5">Muhammad Reza Saputra</h1>
                     <p className="text-lg mb-4">I'm Laravel Developer</p>
@@ -15,24 +13,18 @@ function Home() {
                 <div className="flex justify-center pb-32">
                     <a
                         href="https://www.linkedin.com/in/muhammad-reza-saputra-b6a81726b"
-                        target="_blank"
-                        rel="noopener noreferrer"
                         className="mr-4"
                     >
                         <FaLinkedin size={ 32 } />
                     </a>
                     <a
                         href="https://www.facebook.com/zeinatsu/"
-                        target="_blank"
-                        rel="noopener noreferrer"
                         className="mr-4"
                     >
                         <FaFacebook size={ 32 } />
                     </a>
                     <a
                         href="https://github.com/rizheez"
-                        target="_blank"
-                        rel="noopener noreferrer"
                         className="mr-4"
                     >
                         <FaGithub size={ 32 } />
